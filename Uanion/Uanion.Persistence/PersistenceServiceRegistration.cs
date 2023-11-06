7d66e0a72a5fa40704b13e551e0472de11d7b933
Zhartun <gulyaevdima1998@gmail.com>
2023-11-06 22:03:44 +0200

    Register ProfileRepository to Db and add Profile DbSet to UanionDbContext

--- Uanion/Uanion.Persistence/PersistenceServiceRegistration.cs
+++ Uanion/Uanion.Persistence/PersistenceServiceRegistration.cs
@@ -17,6 +17,8 @@ public static class PersistenceServiceRegistration
 
         services.AddScoped<IUserRepository, UserRepository>();
 
+        services.AddScoped<IProfileRepository, ProfileRepository>();
+
         return services;
     }
 }
