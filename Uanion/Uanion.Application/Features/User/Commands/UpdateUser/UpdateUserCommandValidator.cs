d576abc109013c115e7e861183b6b5c14a38ec1d
Andrii <andrii.kozhevnikov@gmail.com>
2023-11-10 17:41:16 +0200

    Added needed validation for UpdateUserCommand Removed unused usings

--- Uanion/Uanion.Application/Features/User/Commands/UpdateUser/UpdateUserCommandValidator.cs
+++ Uanion/Uanion.Application/Features/User/Commands/UpdateUser/UpdateUserCommandValidator.cs
@@ -7,12 +7,18 @@ public class UpdateUserCommandValidator : AbstractValidator<UpdateUserCommand>
     public UpdateUserCommandValidator()
     {
         RuleFor(p => p.Email)
-            .MaximumLength(255).WithMessage("{PropertyName} must not exceed 255 characters");
+           .NotEmpty().WithMessage("{PropertyName} is required")
+           .NotNull()
+           .MaximumLength(255).WithMessage("{PropertyName} must not exceed 255 characters");
 
         RuleFor(p => p.Password)
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .NotNull()
             .MaximumLength(50).WithMessage("{PropertyName} must not exceed 50 characters");
 
         RuleFor(p => p.Nickname)
+            .NotEmpty().WithMessage("{PropertyName} is required")
+            .NotNull()
             .MaximumLength(255).WithMessage("{PropertyName} must not exceed 255 characters");
 
         RuleFor(p => p.FirstName)
